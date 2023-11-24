50bbeb2c520899aa782b9a8ee31475a6ef61df02
AlifFadhlan <aliffadhlanfadhilah@gmail.com>
2023-11-24 23:48:59 +0700

    membuat list tutorial

---
+++ app/src/main/java/org/d3ifcool/modidemo/ListTutorial.kt
@@ -0,0 +1,11 @@
+package org.d3ifcool.modidemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListTutorial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_tutorial)
+    }
+}
